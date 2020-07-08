1aaf4470bf51fbf2f51dfe49af234f8a9755a323
Crystina <xzhangbx@ust.hk>
2020-07-08 04:52:24 -0400

    support seed; dict.items() typo, call super.init

--- tensorflow_addons/layers/mixout.py
+++ tensorflow_addons/layers/mixout.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 
 @tf.keras.utils.register_keras_serializable(package="Addons")
 class Mixout(tf.keras.layers.Layer):
-    def __init__(self, rate, pretrain_weights):
+    def __init__(self, rate, pretrain_weights, seed=None, **kwargs):
+        super(Mixout, self).__init__()
         if not isinstance(pretrain_weights, dict):
             raise ValueError(f"Unexpect type of pretrain weights: expect dictionary mapping variable name to tf.Tensor, got {type(pretrain_weights)}")
         if not all([isinstance(value, tf.Tensor) or isinstance(value, np.array) for value in pretrain_weights.values()]):
@@ -17,21 +18,22 @@ class Mixout(tf.keras.layers.Layer):
             raise ValueError("rate must be a scalar tensor or a float in the "
                              "range [0, 1), got %g" % rate)
 
-        for name, variable in pretrain_weights:
+        for name, variable in pretrain_weights.items():
             pretrain_weights[name] = tf.convert_to_tensor(variable, name=f"from_pretrain/{name}")
 
         self.rate = rate
+        self.seed = seed
         self.pretrain_weights = pretrain_weights
 
     def call(self, inputs):
         with tf.name_scope("mixout") as name:
             x = tf.convert_to_tensor(inputs, name="x")
-            target_x = self.pretrain_weights.get(inputs.name, None)
 
-            if not target_x:
+            if inputs.name not in self.pretrain_weights:
                 tf.get_logger().warn(f"pre-train weight for {inputs.name} cannot be found given weights, skip.")
                 return x
 
+            target_x = self.pretrain_weights[inputs.name]
             x_dtype = x.dtype
             if not x_dtype.is_floating:
                 raise ValueError("x has to be a floating point tensor since it's going "
@@ -46,7 +48,7 @@ class Mixout(tf.keras.layers.Layer):
                     keep_prob = 1 - self.rate
                     scale = 1 / keep_prob
                     scale = tf.convert_to_tensor(scale, dtype=x_dtype)
-                    ret, tgt_ret = tf.mul(x, scale), tf.mul(target_x, scale)
+                    ret, tgt_ret = tf.multiply(x, scale), tf.multiply(target_x, scale)
                 else:
                     raise ValueError("rate is neither scalar nor scalar tensor %r" % self.rate)
             else:
@@ -59,8 +61,8 @@ class Mixout(tf.keras.layers.Layer):
                             (x_dtype.name, rate_dtype.name, self.rate))
                     rate = tf.cast(self.rate, x_dtype, name="rate")
                 one_tensor = tf.constant(1, dtype=x_dtype)
-                ret = tf.real_div(x, tf.sub(one_tensor, rate))
-                tgt_ret = tf.real_div(target_x, tf.sub(one_tensor, rate))
+                ret = tf.real_div(x, tf.subtract(one_tensor, rate))
+                tgt_ret = tf.real_div(target_x, tf.subtract(one_tensor, rate))
 
             noise_shape = tf.shape(x)
             # Sample a uniform distribution on [0.0, 1.0) and select values larger
@@ -68,13 +70,13 @@ class Mixout(tf.keras.layers.Layer):
             #
             # NOTE: Random uniform can only generate 2^23 floats on [1.0, 2.0)
             # and subtract 1.0.
-            random_tensor = tf.random.uniform(noise_shape, dtype=x_dtype)
+            random_tensor = tf.random.uniform(noise_shape, seed=self.seed, dtype=x_dtype)
             # NOTE: if (1.0 + rate) - 1 is equal to rate, then that float is selected,
             # hence a >= comparison is used.
-            keep_mask = tf.cast(random_tensor >= rate, x_dtype)
-            replace_mask = tf.cast(random_tensor < rate, x_dtype)
-            ret = tf.add(tf.mul(ret, keep_mask), tf.mul(tgt_ret, replace_mask))
-            ret = ret * keep_mask + tgt_ret * replace_mask - rate * tgt_ret
+            keep_mask = tf.cast(random_tensor >= self.rate, x_dtype)
+            replace_mask = tf.cast(random_tensor < self.rate, x_dtype)
+            ret = tf.add(tf.multiply(ret, keep_mask), tf.multiply(tgt_ret, replace_mask))
+            ret = tf.subtract(ret, tf.multiply(self.rate, tgt_ret))
 
             if not is_executing_eagerly:
                 ret.set_shape(x.get_shape())
